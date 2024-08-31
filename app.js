3063a48f95d9eb3c2f696cdc4b0f86febf3fe036
MoonByte29 <moon.byte29@gmail.com>
2024-09-01 03:21:07 +0530

    Commit

--- app.js
+++ app.js
@@ -48,17 +48,62 @@ btn.addEventListener("click", () => {
   recognition.start();
 });
 
-function takeCommand(msg){
-  if(msg.includes('hey')|| msg.includes('hello') || msg.includes('hi')){
+function takeCommand(msg) {
+  if (msg.includes("hey") || msg.includes("hello") || msg.includes("hi")) {
     speak("Hello, how can I assist you?");
   }
-  if(msg.includes("open")){
+  else if (msg.includes("Thank You") || msg.includes("Thanks")) {
+    speak("My pleasue, It feels good to help you!");
+  } else if (msg.includes("calculator")) {
+    window.open("Calculator:///");
+    speak("Opening Calculator");
+  } else if (msg.includes("open")) {
     const domain = msg.split("open")[1].trim();
-    speak("Opening"+ domain);
-    window.open(`https://${domain}com`,"_blank");
+    speak("Opening" + domain);
+    window.open(`https://${domain}com`, "_blank");
+  } else if (msg.includes("time")) {
+    speak(
+      new Date().toLocaleTimeString(undefined, {
+        hour: "numeric",
+        minute: "2-digit",
+      })
+    );
+  } else if (msg.includes("date") || msg.includes("day")) {
+    speak(
+      new Date().toLocaleTimeString(undefined, {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      })
+    );
+  } else if (
+    msg.includes("what is") ||
+    msg.includes("who is") ||
+    msg.includes("which is")
+  ) {
+    window.open(
+      `https://www.google.com/search?q=${msg.replace("", "+")}`,
+      "_blank"
+    );
+    const finalText = "This is what I found on internet regarding" + msg;
+    speak(finalText);
+  } else if (msg.includes("play")) {
+    const song = msg.split("play")[1].trim();
+    speak(`Playing ${song}`);
+    const songUrl = `https://www.youtube.com/results?search_query=${song}`;
+    window.open(songUrl, "_blank");
+  } else if (msg.includes("stop")) {
+    speak("Stopping music");
+    window.open(
+      "https://www.youtube.com/results?search_query=stop+music",
+      "_blank"
+    );
   }
-  if(msg.includes('what is') || msg.includes('who is')  || msg.includes('which is') || msg){
-    window.open(`https://www.google.com/search?q=${msg.replace("","+")}`,'_blank');
+   else if(msg){
+    window.open(
+      `https://www.google.com/search?q=${msg.replace("", "+")}`,
+      "_blank"
+    );
     const finalText = "This is what I found on internet regarding" + msg;
     speak(finalText);
   }
